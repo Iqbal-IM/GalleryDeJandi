aa2e92b98f7e19af26e4bd65470f094da44af66a
Iqball <iqbal.syko97@gmail.com>
2021-11-20 09:59:43 +0800

    d

--- admin/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-error_reporting(~E_NOTICE);
-
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$dbname = 'gallerydejandi';
-$koneksi = mysqli_connect($host, $user, $password, $dbname);
